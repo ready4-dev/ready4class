c040e0ab7b228a49fa65e364d638278f1e5cd6e7
Matthew P Hamilton <matthew.hamilton@orygen.org.au>
2020-09-05 18:13:01 +1000

    Increment version number

--- data-raw/MAKE_CLASSES.R
+++ data-raw/MAKE_CLASSES.R
@@ -1,30 +1,19 @@
 ## This script creates the initial S3 class exported with this package.
 ##
 ## 1. Pre-requisites
-## The following script to make the initial class was run once the following items were already in the package's data-raw/mthds/ folder:
-## fn_write_scripts_to_mk_r3_cls.R
-## fn_write_scripts_to_mk_r4_cls.R
-## import_methods.R
-## import_pipe.R
-## mthd_make_classes.R
-## pckg_ready4class.R
+## The following script to make the initial class was run once methods scripts were already in the package's data-raw/mthds/ folder.
 ##
-## 2. Document and load all package functions.
-# devtools::document()
-# devtools::load_all()
-#ready4fun::read_fns("data-raw/mthds/")
-##
-## 3. Specify details about this package and the prefix we will use on all classes we create in this package.
+## 2. Specify details about this package and the prefix we will use on all classes we create in this package.
 name_pfx_1L_chr <- "ready4_"
-dev_pkg_ns_1L_chr <- "ready4class"
-## 2. Create a table with metadata about frequently used class prototypes
+#dev_pkg_ns_1L_chr <- ready4fun::get_dev_pkg_nm()
+## 3. Create a table with metadata about frequently used class prototypes
 prototype_lup <- tibble::tibble(type_chr = c("character", "list", "logical", "numeric", "POSIXt", "sf","tbl_df"), ## CHANGED FROM POSIXt
                                 val_chr = c("NA_character_", "list(list())","NA","NA_real_",".POSIXct(NA_character_)", "st_sf(sf::st_sfc())","tibble::tibble()"),
                                 pt_ns_chr = c("base", "base", "base", "base", "base", "sf", "tibble"),
                                 fn_to_call_chr = c("", "list", "", "", ".POSIXct", "st_sf", "tibble"),
                                 default_val_chr = c("NA_character_", "list()", "NA", "NA_real_", "NA_character_", "sf::st_sfc()", ""),
                                 old_class_lgl = FALSE)
-## 3. Create a table with the metadata for the ready4_class_make_tb class that we wish to create.
+## 4. Create a table with the metadata for the ready4_class_make_tb class that we wish to create.
 classes_to_make_tb <- tibble::tribble(
   ~ make_s3_lgl, ~ name_stub_chr, ~ pt_ls, ~ pt_chkr_pfx_ls, ~ pt_ns_ls, ~ vals_ls, ~ allowed_vals_ls, ~ min_max_vals_ls, ~ start_end_vals_ls, ~ class_desc_chr, ~ parent_class_chr, ~ slots_ls, ~ meaningful_nms_ls, ~inc_clss_ls,
   TRUE, "class_make_tb", list("tibble"), list("is_"), list("tibble"), list(make_s3_lgl = "logical(0)",
@@ -41,7 +30,7 @@ classes_to_make_tb <- tibble::tribble(
                                                                            slots_ls = "list()",
                                                                            meaningful_nms_ls = "list()",
                                                                            inc_clss_ls = "list()"), NULL, NULL, NULL, "MAKE CLASS TABLE Readyforwhatsnext S3 class of metadata required to make new classes.", NA_character_, NULL, NULL, NULL)
-## 4. Convert the metadata table into a new S3 class called ready4_class_make_tb
+## 5. Convert the metadata table into a new S3 class called ready4_class_make_tb
 write_scripts_to_mk_r3_cls(name_stub_1L_chr = classes_to_make_tb$name_stub_chr,
               name_pfx_1L_chr = name_pfx_1L_chr,
               output_dir_1L_chr = "R",
