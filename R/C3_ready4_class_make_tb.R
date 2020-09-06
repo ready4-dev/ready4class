d1fd5348776cff57fcfad7774599f8567359bdbd
Matthew P Hamilton <matthew.hamilton@orygen.org.au>
2020-09-06 10:21:24 +1000

    Increment version number

--- R/C3_ready4_class_make_tb.R
+++ R/C3_ready4_class_make_tb.R
@@ -1,38 +1,38 @@
 
-#' 
+#' ready4_class_make_tb
 #' @description Create a new valid instance of the S3 class: ready4_class_make_tb
 #' @param x PARAM_DESCRIPTION, Default: make_prototype_ready4_class_make_tb()
 #' @return A validated instance of the ready4_class_make_tb class
 #' @details MAKE CLASS TABLE Readyforwhatsnext S3 class of metadata required to make new classes.
 #' @rdname ready4_class_make_tb
-#' @export 
+#' @export
 
-ready4_class_make_tb <- function(x = make_prototype_ready4_class_make_tb()){ 
+ready4_class_make_tb <- function(x = make_prototype_ready4_class_make_tb()){
 validate_ready4_class_make_tb(new_ready4_class_make_tb(x))
 }
-#' 
+#'
 #' @description Create a new unvalidated instance of the S3 class: new_ready4_class_make_tb
 #' @param x PARAM_DESCRIPTION
 #' @return An unvalidated instance of the ready4_class_make_tb class
 #' @details MAKE CLASS TABLE Readyforwhatsnext S3 class of metadata required to make new classes.
 #' @rdname new_ready4_class_make_tb
-#' @export 
+#' @export
 #' @importFrom tibble is_tibble
-new_ready4_class_make_tb <- function(x){ 
+new_ready4_class_make_tb <- function(x){
 stopifnot(tibble::is_tibble(x))
 class(x) <- append(c("ready4_class_make_tb",setdiff(make_prototype_ready4_class_make_tb() %>% class(),class(x))),
 class(x))
 x
 }
-#' 
+#'
 #' @description Create a new prototype for S3 class: make_prototype_ready4_class_make_tb
 
 #' @return A prototpe for ready4_class_make_tb class
 #' @details MAKE CLASS TABLE Readyforwhatsnext S3 class of metadata required to make new classes.
 #' @rdname make_prototype_ready4_class_make_tb
-#' @export 
+#' @export
 #' @importFrom tibble tibble
-make_prototype_ready4_class_make_tb <- function(){ 
+make_prototype_ready4_class_make_tb <- function(){
 tibble::tibble(make_s3_lgl = logical(0),
 name_stub_chr = character(0),
 pt_ls = list(),
@@ -48,13 +48,13 @@ slots_ls = list(),
 meaningful_nms_ls = list(),
 inc_clss_ls = list())
 }
-#' 
+#'
 #' @description Validate an instance of the S3 class: validate_ready4_class_make_tb
 #' @param x PARAM_DESCRIPTION
 #' @return A prototpe for ready4_class_make_tb class
 #' @details MAKE CLASS TABLE Readyforwhatsnext S3 class of metadata required to make new classes.
 #' @rdname validate_ready4_class_make_tb
-#' @export 
+#' @export
 #' @importFrom stringr str_detect str_c
 #' @importFrom dplyr summarise_all arrange filter pull
 #' @importFrom tidyr gather
@@ -66,34 +66,34 @@ stop(paste0("TIBBLE must include columns named: ",
 names(make_prototype_ready4_class_make_tb()) %>% stringr::str_c(sep="", collapse = ", ")),
 call. = FALSE)
 }
- if(!identical(make_prototype_ready4_class_make_tb() %>% 
-dplyr::summarise_all(class) %>% 
- tidyr::gather(variable,class) %>% 
+ if(!identical(make_prototype_ready4_class_make_tb() %>%
+dplyr::summarise_all(class) %>%
+ tidyr::gather(variable,class) %>%
 dplyr::arrange(variable),
-x %>% 
-dplyr::summarise_all(class) %>% 
- tidyr::gather(variable,class) %>% 
+x %>%
+dplyr::summarise_all(class) %>%
+ tidyr::gather(variable,class) %>%
 dplyr::filter(variable %in% names(make_prototype_ready4_class_make_tb())) %>% dplyr::arrange(variable))){
 stop(paste0("TIBBLE columns should be of the following classes: ",
-purrr::map2_chr(make_prototype_ready4_class_make_tb() %>% 
-dplyr::summarise_all(class) %>% 
- tidyr::gather(variable,class) %>% 
+purrr::map2_chr(make_prototype_ready4_class_make_tb() %>%
+dplyr::summarise_all(class) %>%
+ tidyr::gather(variable,class) %>%
 dplyr::pull(1),
- make_prototype_ready4_class_make_tb() %>% 
-dplyr::summarise_all(class) %>% 
- tidyr::gather(variable,class) %>% 
+ make_prototype_ready4_class_make_tb() %>%
+dplyr::summarise_all(class) %>%
+ tidyr::gather(variable,class) %>%
 dplyr::pull(2),
- ~ paste0(.x,": ",.y)) %>% 
+ ~ paste0(.x,": ",.y)) %>%
 stringr::str_c(sep="", collapse = ", ")),
 call. = FALSE)
 }
 x}
-#' 
+#'
 #' @description Check whether an object is a valid instance of the S3 class: is_ready4_class_make_tb
 #' @param x PARAM_DESCRIPTION
 #' @return A logical value, TRUE if a valid instance of the ready4_class_make_tb class
 #' @details MAKE CLASS TABLE Readyforwhatsnext S3 class of metadata required to make new classes.
 #' @rdname is_ready4_class_make_tb
-#' @export 
+#' @export
 
 is_ready4_class_make_tb <- function(x) inherits(validate_ready4_class_make_tb(x), "ready4_class_make_tb")
