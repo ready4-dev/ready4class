b2e98c807c45b4de29600fb05653017a3cbb5d8c
Matthew P Hamilton <matthew.hamilton@orygen.org.au>
2020-09-13 18:26:40 +1000

    Added links to website

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -126,6 +126,9 @@ ready4fun::write_and_doc_fn_fls(fns_dmt_tb = all_fns_dmt_tb,
                                 r_dir_1L_chr = "R",
                                 dev_pkgs_chr = "ready4fun",
                                 update_pkgdown_1L_lgl = T)
+ready4fun::write_links_for_website(user_manual_url_1L_chr = "https://readyforwhatsnext.github.io/readyforwhatsnext/pdfs/ready4class_0.0.0.9138.pdf",
+                          developer_manual_url_1L_chr = "https://readyforwhatsnext.github.io/readyforwhatsnext/pdfs/ready4class_0.0.0.9138_dev.pdf")
+
 #pkgdown::build_site()
 # 15. Manual step: Push changes
 ## NOTE TO SELF: Need to implement variant of local git step outlined here: https://pkgdown.r-lib.org/reference/deploy_site_github.html
