0907484182b44d67a33e6ce40f289af26ad27b63
Matthew P Hamilton <matthew.hamilton@orygen.org.au>
2020-10-07 16:28:55 +1100

    Added S4 method setting

--- R/grp_generics.R
+++ R/grp_generics.R
@@ -3,9 +3,9 @@
 #' @description add_class() is an Add Class generic that adds information about a class.
 #' @param x An object
 #' @param ... Additional arguments
-#' @export 
+#' @export
 
-add_class <- function (x, ...) 
+add_class <- function (x, ...)
 {
     UseMethod("add_class", x)
 }
@@ -15,9 +15,9 @@ methods::setGeneric("add_class")
 #' @description make_lup() is a Make Lookup Table generic that applies a Make method and then updates the output of that method.
 #' @param x An object
 #' @param ... Additional arguments
-#' @export 
+#' @export
 
-make_lup <- function (x, ...) 
+make_lup <- function (x, ...)
 {
     UseMethod("make_lup", x)
 }
@@ -27,9 +27,9 @@ methods::setGeneric("make_lup")
 #' @description order_tb() is an Order Tibble generic that orders a tibble.
 #' @param x An object
 #' @param ... Additional arguments
-#' @export 
+#' @export
 
-order_tb <- function (x, ...) 
+order_tb <- function (x, ...)
 {
     UseMethod("order_tb", x)
 }
@@ -39,9 +39,9 @@ methods::setGeneric("order_tb")
 #' @description remake_ls_cols() is a Remake List Columns generic that remakes list columns.
 #' @param x An object
 #' @param ... Additional arguments
-#' @export 
+#' @export
 
-remake_ls_cols <- function (x, ...) 
+remake_ls_cols <- function (x, ...)
 {
     UseMethod("remake_ls_cols", x)
 }
@@ -52,9 +52,9 @@ methods::setGeneric("remake_ls_cols")
 #' @param x An object
 #' @param attached_nss_chr Attached namespaces (a character vector)
 #' @param ... Additional arguments
-#' @export 
+#' @export
 
-update_lup_for_ns <- function (x, attached_nss_chr, ...) 
+update_lup_for_ns <- function (x, attached_nss_chr, ...)
 {
     UseMethod("update_lup_for_ns", x)
 }
@@ -64,9 +64,9 @@ methods::setGeneric("update_lup_for_ns")
 #' @description write_classes() is a Write Classes generic that writes new classes.
 #' @param x An object
 #' @param ... Additional arguments
-#' @export 
+#' @export
 
-write_classes <- function (x, ...) 
+write_classes <- function (x, ...)
 {
     UseMethod("write_classes", x)
 }
@@ -76,9 +76,9 @@ methods::setGeneric("write_classes")
 #' @description write_classes_and_make_lup() is a Write Classes and Make Lookup Table generic that makes new classes and creates or updates a class prototype lookup table.
 #' @param x An object
 #' @param ... Additional arguments
-#' @export 
+#' @export
 
-write_classes_and_make_lup <- function (x, ...) 
+write_classes_and_make_lup <- function (x, ...)
 {
     UseMethod("write_classes_and_make_lup", x)
 }
--- R/mthd_write_classes.R
+++ R/mthd_write_classes.R
@@ -11,37 +11,37 @@
 #' @param abbreviations_lup Abbreviations (a lookup table), Default: NULL
 #' @return NULL
 #' @rdname write_classes.ready4_constructor_tbl
-#' @export 
+#' @export
 #' @importFrom purrr pwalk
 #' @importFrom dplyr filter
-write_classes.ready4_constructor_tbl <- function (x, name_pfx_1L_chr, output_dir_1L_chr, file_exists_cdn_1L_chr = NULL, 
-    prototype_lup = NULL, nss_to_ignore_chr, req_pkgs_chr = NA_character_, 
-    class_in_cache_cdn_1L_chr = "stop", abbreviations_lup = NULL) 
+write_classes.ready4_constructor_tbl <- function (x, name_pfx_1L_chr, output_dir_1L_chr, file_exists_cdn_1L_chr = NULL,
+    prototype_lup = NULL, nss_to_ignore_chr, req_pkgs_chr = NA_character_,
+    class_in_cache_cdn_1L_chr = "stop", abbreviations_lup = NULL)
 {
-    if (is.null(abbreviations_lup)) 
+    if (is.null(abbreviations_lup))
         data("abbreviations_lup", package = "ready4class", envir = environment())
-    purrr::pwalk(x %>% dplyr::filter(make_s3_lgl == T), ~write_scripts_to_mk_r3_cls(name_stub_1L_chr = ..2, 
-        name_pfx_1L_chr = name_pfx_1L_chr, output_dir_1L_chr = output_dir_1L_chr, 
+    purrr::pwalk(x %>% dplyr::filter(make_s3_lgl == T), ~write_scripts_to_mk_r3_cls(name_stub_1L_chr = ..2,
+        name_pfx_1L_chr = name_pfx_1L_chr, output_dir_1L_chr = output_dir_1L_chr,
         class_desc_1L_chr = ..10, parent_cls_nm_1L_chr = if (is.na(..11)) {
             NULL
         }
         else {
             ..11
-        }, type_1L_chr = ..3[[1]], pt_chkr_pfx_1L_chr = ..4[[1]], 
-        pt_ns_1L_chr = ifelse(..5[[1]] %in% c("base"), "", ..5[[1]]), 
-        vals_ls = ..6, allowed_vals_ls = ..7, min_max_vals_dbl = ..8, 
-        start_end_vals_dbl = ..9, file_exists_cdn_1L_chr = file_exists_cdn_1L_chr, 
-        prototype_lup = prototype_lup, nss_to_ignore_chr = nss_to_ignore_chr, 
+        }, type_1L_chr = ..3[[1]], pt_chkr_pfx_1L_chr = ..4[[1]],
+        pt_ns_1L_chr = ifelse(..5[[1]] %in% c("base"), "", ..5[[1]]),
+        vals_ls = ..6, allowed_vals_ls = ..7, min_max_vals_dbl = ..8,
+        start_end_vals_dbl = ..9, file_exists_cdn_1L_chr = file_exists_cdn_1L_chr,
+        prototype_lup = prototype_lup, nss_to_ignore_chr = nss_to_ignore_chr,
         abbreviations_lup = abbreviations_lup))
-    purrr::pwalk(x %>% dplyr::filter(make_s3_lgl != T), ~write_scripts_to_mk_r4_cls(name_stub_1L_chr = ..2, 
-        name_pfx_1L_chr = name_pfx_1L_chr, output_dir_1L_chr = output_dir_1L_chr, 
+    purrr::pwalk(x %>% dplyr::filter(make_s3_lgl != T), ~write_scripts_to_mk_r4_cls(name_stub_1L_chr = ..2,
+        name_pfx_1L_chr = name_pfx_1L_chr, output_dir_1L_chr = output_dir_1L_chr,
         class_desc_1L_chr = ..10, parent = if (is.na(..11)) {
             NULL
         }
         else {
             ..11
-        }, slots_chr = ..12[[1]], type_chr = ..3[[1]], meaningful_nms_ls = ..13, 
-        vals_ls = ..6[[1]], allowed_vals_ls = ..7[[1]], clss_to_inc_chr = ..14[[1]], 
-        prototype_lup = prototype_lup, nss_to_ignore_chr = nss_to_ignore_chr, 
+        }, slots_chr = ..12[[1]], type_chr = ..3[[1]], meaningful_nms_ls = ..13,
+        vals_ls = ..6[[1]], allowed_vals_ls = ..7[[1]], clss_to_inc_chr = ..14[[1]],
+        prototype_lup = prototype_lup, nss_to_ignore_chr = nss_to_ignore_chr,
         req_pkgs_chr = req_pkgs_chr, class_in_cache_cdn_1L_chr = class_in_cache_cdn_1L_chr))
 }
