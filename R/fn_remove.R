bd7408aa7772cb64138553536aecc5415241e343
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2020-12-06 10:36:33 +1100

    Fix of adding ns to parent cls

--- R/fn_remove.R
+++ R/fn_remove.R
@@ -6,6 +6,7 @@
 #' @rdname remove_class_from_cache
 #' @export 
 #' @importFrom methods isClass removeClass
+#' @keywords internal
 remove_class_from_cache <- function (class_nm_1L_chr, class_in_cache_cdn_1L_chr = "stop") 
 {
     keep_going_1L_lgl <- methods::isClass(class_nm_1L_chr)
