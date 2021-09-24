328f5259effe1c911573bfe534a91f905ae7f64d
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-09-24 16:57:40 +1000

    bug fix in author mthd

--- R/mthd_author.R
+++ R/mthd_author.R
@@ -90,15 +90,15 @@ author.ready4class_manifest <- function (x, dv_url_pfx_1L_chr = NULL, init_class
         x$pkg_setup_r3$subsequent_ls$prototype_lup <- init_class_pt_lup
     }
     x$pkg_setup_r3$subsequent_ls$cls_fn_ls <- ready4fun::make_pt_ready4fun_fn_ls(args_ls = list(x = x$constructor_tbl_r3, 
-        dev_pkg_ns_1L_chr = x$initial_ls$pkg_desc_ls$Package, 
-        name_pfx_1L_chr = paste0(x$initial_ls$pkg_desc_ls$Package, 
-            "_"), output_dir_1L_chr = paste0(x$initial_ls$path_to_pkg_rt_1L_chr, 
+        dev_pkg_ns_1L_chr = x$pkg_setup_r3$initial_ls$pkg_desc_ls$Package, 
+        name_pfx_1L_chr = paste0(x$pkg_setup_r3$initial_ls$pkg_desc_ls$Package, 
+            "_"), output_dir_1L_chr = paste0(x$pkg_setup_r3$initial_ls$path_to_pkg_rt_1L_chr, 
             "/R"), delete_cdn_ptrn_chr = NA_character_, file_exists_cdn_1L_chr = "overwrite", 
         init_class_pt_lup = init_class_pt_lup, nss_to_ignore_chr = nss_to_ignore_chr, 
         req_pkgs_chr = req_pkgs_chr, class_in_cache_cdn_1L_chr = "stop", 
-        abbreviations_lup = x$subsequent_ls$abbreviations_lup, 
-        object_type_lup = x$subsequent_ls$object_type_lup), fn = author.ready4class_constructor_tbl) %>% 
-        ready4fun::ready4fun_fn_ls()
+        abbreviations_lup = x$pkg_setup_r3$subsequent_ls$abbreviations_lup, 
+        object_type_lup = x$pkg_setup_r3$subsequent_ls$object_type_lup), 
+        fn = author.ready4class_constructor_tbl) %>% ready4fun::ready4fun_fn_ls()
     x_ready4fun_pkg_setup <- ready4fun::author(x$pkg_setup_r3, 
         dv_url_pfx_1L_chr = dv_url_pfx_1L_chr, key_1L_chr = key_1L_chr, 
         list_generics_1L_lgl = list_generics_1L_lgl, self_serve_1L_lgl = self_serve_1L_lgl, 
