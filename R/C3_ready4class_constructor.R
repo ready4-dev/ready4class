7aa4cc4fb5c462f8987443b6096fb3ba0410441d
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-12-02 11:43:56 +1100

    redo of s3 dmtn

--- R/C3_ready4class_constructor.R
+++ R/C3_ready4class_constructor.R
@@ -6,8 +6,8 @@ setOldClass(c("ready4class_constructor","tbl_df", "tbl", "data.frame"))
 #' @return A validated instance of the ready4 S3 class Constructor Table.
 #' @details ready4 S3 class Constructor Table.
 #' @rdname ready4class_constructor
-#' @export
-ready4class_constructor <- function(x = make_pt_ready4class_constructor()){
+#' @export 
+ready4class_constructor <- function(x = make_pt_ready4class_constructor()){ 
 validate_ready4class_constructor(make_new_ready4class_constructor(x))
 }
 #' make new ready4class package constructor ready4 S3 class Constructor Table.
@@ -15,17 +15,17 @@ validate_ready4class_constructor(make_new_ready4class_constructor(x))
 #' @param x A prototype for the ready4 S3 class Constructor Table.
 #' @return An unvalidated instance of the ready4 S3 class Constructor Table.
 #' @details ready4 S3 class Constructor Table.
-#' @rdname ready4class_constructor
-#' @export
+#' @rdname make_new_ready4class_constructor
+#' @export 
 #' @importFrom tibble is_tibble
-make_new_ready4class_constructor <- function(x){
+#' @keywords internal
+make_new_ready4class_constructor <- function(x){ 
 stopifnot(tibble::is_tibble(x))
 class(x) <- append(c("ready4class_constructor",setdiff(make_pt_ready4class_constructor() %>% class(),class(x))),
 class(x))
 x
 }
 #' make prototype ready4class package constructor ready4 S3 class Constructor Table.
-#' @description Create a new prototype for the ready4 S3 class Constructor Table.
 #' @param make_s3_lgl Make S3 (a logical vector), Default: logical(0)
 #' @param name_stub_chr Name stub (a character vector), Default: character(0)
 #' @param pt_ls Prototype (a list), Default: list()
@@ -42,9 +42,9 @@ x
 #' @param inc_clss_ls Include classes (a list), Default: list()
 #' @param asserts_ls Asserts (a list), Default: list()
 #' @return A prototype for ready4 S3 class Constructor Table.
-#' @details ready4 S3 class Constructor Table.
+#' 
 #' @rdname ready4class_constructor
-#' @export
+#' @export 
 #' @importFrom ready4 update_pt_fn_args_ls
 #' @importFrom rlang exec
 #' @importFrom tibble tibble
@@ -62,7 +62,7 @@ parent_class_chr = character(0),
 slots_ls = list(),
 meaningful_nms_ls = list(),
 inc_clss_ls = list(),
-asserts_ls = list()){
+asserts_ls = list()){ 
 args_ls <- list(make_s3_lgl = make_s3_lgl,
 name_stub_chr = name_stub_chr,
 pt_ls = pt_ls,
@@ -85,12 +85,13 @@ rlang::exec(tibble::tibble,!!!args_ls)
 #' @param x An unvalidated instance of the ready4 S3 class Constructor Table.
 #' @return A prototpe for ready4 S3 class Constructor Table.
 #' @details ready4 S3 class Constructor Table.
-#' @rdname ready4class_constructor
-#' @export
+#' @rdname validate_ready4class_constructor
+#' @export 
 #' @importFrom stringr str_detect str_c
 #' @importFrom dplyr summarise_all filter arrange pull
 #' @importFrom tidyr gather
 #' @importFrom purrr map_chr map2_chr
+#' @keywords internal
 validate_ready4class_constructor <- function(x){
 if(sum(stringr::str_detect(names(x)[names(x) %in% names(make_pt_ready4class_constructor())],
 names(make_pt_ready4class_constructor())))!=length(names(make_pt_ready4class_constructor()))){
@@ -99,29 +100,29 @@ names(make_pt_ready4class_constructor()) %>% stringr::str_c(sep="", collapse = "
 call. = FALSE)
 }
 
- if(!identical(make_pt_ready4class_constructor() %>%
-dplyr::summarise_all(class) %>%
- tidyr::gather(variable,class) %>%
- dplyr::filter(!is.na(class)) %>%
+ if(!identical(make_pt_ready4class_constructor() %>% 
+dplyr::summarise_all(class) %>% 
+ tidyr::gather(variable,class) %>% 
+ dplyr::filter(!is.na(class)) %>% 
 dplyr::arrange(variable),
-x %>%
-dplyr::summarise_all(class) %>%
- tidyr::gather(variable,class) %>%
- dplyr::filter(!is.na(class)) %>%
+x %>% 
+dplyr::summarise_all(class) %>% 
+ tidyr::gather(variable,class) %>% 
+ dplyr::filter(!is.na(class)) %>% 
 dplyr::filter(variable %in% names(make_pt_ready4class_constructor())) %>% dplyr::arrange(variable))){
 stop(paste0("TIBBLE columns should be of the following classes: ",
 "",
 {
-class_lup <- make_pt_ready4class_constructor() %>%
-dplyr::summarise_all(class) %>%
- tidyr::gather(variable,class) %>%
+class_lup <- make_pt_ready4class_constructor() %>% 
+dplyr::summarise_all(class) %>% 
+ tidyr::gather(variable,class) %>% 
  dplyr::filter(!is.na(class))
   vars_chr <- class_lup %>% dplyr::pull(1) %>% unique()
   classes_chr <- vars_chr %>%  purrr::map_chr(~dplyr::filter(class_lup, variable == .x) %>%  dplyr::pull(2) %>% paste0(collapse = ", "))
 purrr::map2_chr(vars_chr,
 classes_chr,
-~ paste0(.x,": ",.y)) %>%
-stringr::str_c(sep="", collapse = ",
+~ paste0(.x,": ",.y)) %>% 
+stringr::str_c(sep="", collapse = ", 
 ")
 }),
 call. = FALSE)
@@ -129,10 +130,9 @@ call. = FALSE)
 
 x}
 #' is ready4class package constructor ready4 S3 class Constructor Table.
-#' @description Check whether an object is a valid instance of the ready4 S3 class Constructor Table.
 #' @param x An object of any type
 #' @return A logical value, TRUE if a valid instance of the ready4 S3 class Constructor Table.
-#' @details ready4 S3 class Constructor Table.
+#' 
 #' @rdname ready4class_constructor
-#' @export
+#' @export 
 is_ready4class_constructor <- function(x) inherits(validate_ready4class_constructor(x), "ready4class_constructor")
--- R/C3_ready4class_manifest.R
+++ R/C3_ready4class_manifest.R
@@ -5,8 +5,8 @@
 #' @return A validated instance of the ready4 S3 class Manifest.
 #' @details ready4 S3 class Manifest.
 #' @rdname ready4class_manifest
-#' @export
-ready4class_manifest <- function(x = make_pt_ready4class_manifest()){
+#' @export 
+ready4class_manifest <- function(x = make_pt_ready4class_manifest()){ 
 validate_ready4class_manifest(make_new_ready4class_manifest(x))
 }
 #' make new ready4class package manifest ready4 S3 class Manifest.
@@ -14,27 +14,27 @@ validate_ready4class_manifest(make_new_ready4class_manifest(x))
 #' @param x A prototype for the ready4 S3 class Manifest.
 #' @return An unvalidated instance of the ready4 S3 class Manifest.
 #' @details ready4 S3 class Manifest.
-#' @rdname ready4class_manifest
-#' @export
-make_new_ready4class_manifest <- function(x){
+#' @rdname make_new_ready4class_manifest
+#' @export 
+#' @keywords internal
+make_new_ready4class_manifest <- function(x){ 
 stopifnot(is.list(x))
 class(x) <- append(c("ready4class_manifest",setdiff(make_pt_ready4class_manifest() %>% class(),class(x))),
 class(x))
 x
 }
 #' make prototype ready4class package manifest ready4 S3 class Manifest.
-#' @description Create a new prototype for the ready4 S3 class Manifest.
 #' @param x_ready4fun_manifest PARAM_DESCRIPTION, Default: ready4fun::ready4fun_manifest()
 #' @param constructor_r3 Constructor (a ready4 S3), Default: ready4class_constructor()
 #' @return A prototype for ready4 S3 class Manifest.
-#' @details ready4 S3 class Manifest.
+#' 
 #' @rdname ready4class_manifest
-#' @export
+#' @export 
 #' @importFrom ready4fun ready4fun_manifest
 #' @importFrom ready4 update_pt_fn_args_ls
 #' @importFrom rlang exec
 make_pt_ready4class_manifest <- function(x_ready4fun_manifest = ready4fun::ready4fun_manifest(),
-constructor_r3 = ready4class_constructor()){
+constructor_r3 = ready4class_constructor()){ 
 args_ls <- list(x_ready4fun_manifest = x_ready4fun_manifest,
 constructor_r3 = constructor_r3) %>% ready4::update_pt_fn_args_ls()
 rlang::exec(list,!!!args_ls)
@@ -44,14 +44,15 @@ rlang::exec(list,!!!args_ls)
 #' @param x An unvalidated instance of the ready4 S3 class Manifest.
 #' @return A prototpe for ready4 S3 class Manifest.
 #' @details ready4 S3 class Manifest.
-#' @rdname ready4class_manifest
-#' @export
+#' @rdname validate_ready4class_manifest
+#' @export 
 #' @importFrom stringr str_detect str_c
 #' @importFrom ready4 transform_cls_type_ls
 #' @importFrom tibble as_tibble
 #' @importFrom tidyr gather
 #' @importFrom dplyr filter arrange pull
 #' @importFrom purrr map_chr map2_chr
+#' @keywords internal
 validate_ready4class_manifest <- function(x){
 if(sum(stringr::str_detect(names(x)[names(x) %in% names(make_pt_ready4class_manifest())],
 names(make_pt_ready4class_manifest())))!=length(names(make_pt_ready4class_manifest()))){
@@ -60,29 +61,29 @@ names(make_pt_ready4class_manifest()) %>% stringr::str_c(sep="", collapse = ", "
 call. = FALSE)
 }
 
- if(!identical(make_pt_ready4class_manifest() %>%
-lapply(class) %>% ready4::transform_cls_type_ls() %>% tibble::as_tibble() %>%
- tidyr::gather(variable,class) %>%
- dplyr::filter(!is.na(class)) %>%
+ if(!identical(make_pt_ready4class_manifest() %>% 
+lapply(class) %>% ready4::transform_cls_type_ls() %>% tibble::as_tibble() %>% 
+ tidyr::gather(variable,class) %>% 
+ dplyr::filter(!is.na(class)) %>% 
 dplyr::arrange(variable),
-x %>%
-lapply(class) %>% ready4::transform_cls_type_ls() %>% tibble::as_tibble() %>%
- tidyr::gather(variable,class) %>%
- dplyr::filter(!is.na(class)) %>%
+x %>% 
+lapply(class) %>% ready4::transform_cls_type_ls() %>% tibble::as_tibble() %>% 
+ tidyr::gather(variable,class) %>% 
+ dplyr::filter(!is.na(class)) %>% 
 dplyr::filter(variable %in% names(make_pt_ready4class_manifest())) %>% dplyr::arrange(variable))){
 stop(paste0("LIST elements should be of the following classes: ",
 "",
 {
-class_lup <- make_pt_ready4class_manifest() %>%
-lapply(class) %>% ready4::transform_cls_type_ls() %>% tibble::as_tibble() %>%
- tidyr::gather(variable,class) %>%
+class_lup <- make_pt_ready4class_manifest() %>% 
+lapply(class) %>% ready4::transform_cls_type_ls() %>% tibble::as_tibble() %>% 
+ tidyr::gather(variable,class) %>% 
  dplyr::filter(!is.na(class))
   vars_chr <- class_lup %>% dplyr::pull(1) %>% unique()
   classes_chr <- vars_chr %>%  purrr::map_chr(~dplyr::filter(class_lup, variable == .x) %>%  dplyr::pull(2) %>% paste0(collapse = ", "))
 purrr::map2_chr(vars_chr,
 classes_chr,
-~ paste0(.x,": ",.y)) %>%
-stringr::str_c(sep="", collapse = ",
+~ paste0(.x,": ",.y)) %>% 
+stringr::str_c(sep="", collapse = ", 
 ")
 }),
 call. = FALSE)
@@ -90,10 +91,9 @@ call. = FALSE)
 
 x}
 #' is ready4class package manifest ready4 S3 class Manifest.
-#' @description Check whether an object is a valid instance of the ready4 S3 class Manifest.
 #' @param x An object of any type
 #' @return A logical value, TRUE if a valid instance of the ready4 S3 class Manifest.
-#' @details ready4 S3 class Manifest.
+#' 
 #' @rdname ready4class_manifest
-#' @export
+#' @export 
 is_ready4class_manifest <- function(x) inherits(validate_ready4class_manifest(x), "ready4class_manifest")
